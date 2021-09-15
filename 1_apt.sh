6626b80a01623a0b8a03dfca3d84a30ff48c511e
Ivan <wuyifanwd@hotmail.com>
2021-09-15 16:51:13 +0800

    Update some app installations.

---
+++ 1_apt.sh
@@ -0,0 +1,19 @@
+sudo apt update
+
+sudo apt dist-upgrade
+
+sudo apt install zsh gnome-tweaks openssl  openssh-server pigz git htop vim libxml2-dev libcurl4-openssl-dev
+
+sudo apt install nfs-common ifconfig nfs-utils iperf3
+
+sudo apt install r-base r-base-core r-base-core-dbg r-cran-curl rstudio-server
+
+sudo apt install cuda libbz2-dev build-essential autoconf automake libtool
+
+sudo apt update
+
+sudo apt dist-upgrade
+
+sudo apt autoremove
+
+sudo apt autoclean
---
+++ 2_brew.sh
@@ -0,0 +1,6 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew tap brewsci/bio
+
+brew install minimap2 kraken2 igv GATK picard-tools bwa-mem2 samtools hisat2 fastqc bowtie2 kmergenie
+
